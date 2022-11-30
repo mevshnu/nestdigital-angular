845405614ce7303c704c8d9704d3f80d2ce4cde4
mevshnu <vishnusivan1000@gmail.com>
2022-11-30 12:25:43 +0530

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GalaryComponent } from './galary/galary.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavComponent } from './nav/nav.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { ViewcourseComponent } from './viewcourse/viewcourse.component';
+import { ViewfriendComponent } from './viewfriend/viewfriend.component';
+import { EmployeeloginComponent } from './employeelogin/employeelogin.component';
+import { EmployeesignupComponent } from './employeesignup/employeesignup.component';
+import { AddfriendComponent } from './addfriend/addfriend.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GalaryComponent,
+    AboutComponent,
+    ContactComponent,
+    NavComponent,
+    AdminloginComponent,
+    ViewcourseComponent,
+    ViewfriendComponent,
+    EmployeeloginComponent,
+    EmployeesignupComponent,
+    AddfriendComponent,
+    AddcourseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employeesignup/employeesignup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeesignup',
+  templateUrl: './employeesignup.component.html',
+  styleUrls: ['./employeesignup.component.css']
+})
+export class EmployeesignupComponent {
+
+}
